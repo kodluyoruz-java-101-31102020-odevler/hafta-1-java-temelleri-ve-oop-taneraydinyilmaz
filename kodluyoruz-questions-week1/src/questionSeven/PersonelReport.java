f901d2ad71f2ba4ea2ef58aabf94bc078354bedb
Aydnylmz <Aydnylmz@Aydnylmz>
2020-12-01 19:35:01 +0300

    md5

--- kodluyoruz-questions-week1/src/questionSeven/PersonelReport.java
+++ kodluyoruz-questions-week1/src/questionSeven/PersonelReport.java
@@ -1,5 +1,9 @@
-package questionSeven;
+package questionSeven; 
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Date;
 
 public class PersonelReport extends Report {
@@ -8,13 +12,11 @@ public class PersonelReport extends Report {
 		super(name, result, createDate);
 	}
 
-	 public String hashMD5Result() {
-		
-		result.hashCode();
-		return result;
-		
-	}
+	public String hashMD5Result() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest md = MessageDigest.getInstance("MD5"); 
+		byte[] md5 = md.digest(result.getBytes("UTF-8"));
+		return md5.toString();
 
- 
+	}
 
 }
